685af1dd7d90f4628f34d17ce2ea9afe1b8b84f2
Rams Gallo <ramsjustin013@gmail.com>
2025-05-04 23:31:01 +0800

    secured secret key

--- board/__init__.py
+++ board/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from dotenv import load_dotenv
 from board import pages
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'iliekturtles'
+    app.secret_key = os.getenv('FLASK_SECRET_KEY')
     app.register_blueprint(pages.bp)
-    
+
     return app
